3969a1cd4ba65e2fa61e7908f9d1a809ce3d0704
srujana2105 <kattasrujana98@gmail.com>
2018-03-20 23:05:15 +0530

    Add files via upload

---
+++ ViewQuestions.php
@@ -0,0 +1,47 @@
+<html>
+    <head>
+    </head>
+    <body>
+<?php 
+
+include 'connection.php';
+$sql="SELECT * FROM testdb.questions";
+$result=mysqli_query($con,$sql);
+$res=mysqli_fetch_all($result,MYSQLI_ASSOC);
+echo "<table border=2>
+        <tr>
+            <th>Question ID</th>
+            <th>Question</th>
+            <th>A</th>
+            <th>B</th>
+            <th>C</th>
+            <th>D</th>
+            <th>Answer</th>
+        </tr>   
+        
+";
+        $r=sizeof($res);
+        for( $i = 0; $i<$r; $i++ ) {
+            echo "<tr>
+            <td>".$res[$i]["QID"]."</td>
+            <td>".$res[$i]["Question"]."</td>
+            <td>".$res[$i]["OpA"]."</td>
+            <td>".$res[$i]["OpB"]."</td>
+            <td>".$res[$i]["OpC"]."</td>
+            <td>".$res[$i]["OpD"]."</td>
+            <td>".$res[$i]["Answer"]."</td>
+            <td><button onclick=edit(".$res[$i]['QID'].")>Edit</button></td>
+          </tr>
+            ";
+         }
+        echo "</table>";
+?>
+        <script>
+        
+        function edit(var q) {
+            
+        }
+        
+        </script>
+        </body>
+</html>
